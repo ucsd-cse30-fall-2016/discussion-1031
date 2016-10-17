ff433d90c8965ca1e388ecf165a42d10dfb0dd3c
Diba Mirza <dimirza@eng.ucsd.edu>
2016-10-17 13:52:50 -0700

    array of size 1

--- pointers.c
+++ pointers.c
@@ -4,19 +4,48 @@
 
 void find_largest_two(int a[], int n, int *largest, int * sec_largest){
 
-   *largest = *sec_largest = a[0];
+   *largest = a[0];
+   *sec_largest = a[n-1];
+   
+   for (int i =1; i<n ; i++) {
+ 
+        if (a[i]> *largest) {
+             *sec_largest = *largest; 
+            *largest = a[i];
+        }
+            
+   }
    return;
 }
 
 
 void main(){
 
-int arr[]={8};
+int arr[]={8, 3, 7, 7, 8, 7, 3};
 int max, sec_max;
 
 find_largest_two( arr, 1, &max, & sec_max);
 assert( max == 8);
 assert(sec_max ==8);
 
+
+find_largest_two( arr, 2, &max, & sec_max);
+assert( max == 8);
+assert(sec_max ==3);
+
+
+find_largest_two( arr+1, 2, &max, & sec_max);
+assert( max == 7);
+assert(sec_max ==3);
+
+
+
+find_largest_two( arr+4, 3, &max, & sec_max);
+assert( max == 8);
+assert(sec_max ==7);
+
+find_largest_two( arr+2, 2, &max, & sec_max);
+assert( max == 7);
+assert(sec_max ==7);
 printf("\n Passed all tests \n");
 }
